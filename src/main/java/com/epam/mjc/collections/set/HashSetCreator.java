b204c59f5d4ad6938f2ee0293a7f1b78269ad746
madara <ranel28021998@gmail.com>
2023-02-08 12:46:05 +0300

    It's done!

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,19 +1,18 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
-        HashSet<Integer> hashSet = new HashSet<>();
-        Iterator<Integer> iterator = sourceList.iterator();
+        HashSet<Integer> hashSet = new LinkedHashSet<>();
 
-        for (int i = 0; i < sourceList.size(); i++) {
-            Integer temp = sourceList.get(i);
+        for (Integer temp : sourceList) {
             if (temp % 2 == 0) {
                 hashSet.add(temp);
                 temp /= 2;
+                if (temp % 2 == 1) {
+                    hashSet.add(temp);
+                }
                 while (temp % 2 == 0) {
                     hashSet.add(temp);
                     temp /= 2;
